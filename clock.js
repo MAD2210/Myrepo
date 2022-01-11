4acc72cb76c80da57ab6a8efa3753011e78b3305
MAD2210 <97533940+MAD2210@users.noreply.github.com>
2022-01-11 20:16:06 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,18 @@
+setInterval(() => {
+    dt = new Date();
+    htime = dt.getHours();
+    mtime = dt.getMinutes();
+    stime = dt.getSeconds();
+    hrotation = 30 * htime + mtime/2;
+    mrotation = 6 * mtime;
+    srotation = 6 * stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
+
+
+
+
+
